3b64ec8cdaa03131ec2c9423027a1e56d2159111
FarukMia79 <farukmia7979@gmail.com>
2026-08-14 20:09:21 +0600

    Activated fresh migration route to clean up database

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/run-migrate', function () {
     try {
         Artisan::call('migrate:fresh --force');
-        return "Database wiped and migrated successfully! Now your site is ready.";
+        return "Database wiped and migrated successfully! Now your photocard site is ready.";
     } catch (\Exception $e) {
         return "Migration Error: " . $e->getMessage();
     }
 });
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 
 Route::get('/{any}', function () {
