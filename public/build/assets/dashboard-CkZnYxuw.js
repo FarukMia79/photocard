539f92c41cfbe9aafbf23764db945da2209b00c8
FarukMia79 <farukmia7979@gmail.com>
2026-08-17 12:58:40 +0600

    add build

--- public/build/assets/dashboard-CkZnYxuw.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as o,a as r,o as s}from"./app-pni5-cK4.js";const n={};function t(c,e){return s(),o("div",null,[...e[0]||(e[0]=[r("h1",null,"Dashboard",-1)])])}const l=a(n,[["render",t]]);export{l as default};
